fe8753b5a5494e1171c9e59236f6a37dcb0423cc
Ishan Singh <isingh@indxx.com>
2025-05-14 20:26:29 +0530

    Created First API View

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = 'https://httpbin.org/status/200'
-endpoint ='https://httpbin.org'
+# endpoint = 'https://httpbin.org/status/200'
+endpoint ='http://127.0.0.1:8000/api'
 
 get_resonse = requests.get(endpoint)
-print(get_resonse.text) 
+print(get_resonse.json()) 
