b026d041f3aa38c92c44e4c8c71a69dc43a7436d
Ishan Singh <isingh@indxx.com>
2025-05-15 11:23:05 +0530

    Django Model Instance as API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,15 @@
 from django.http import JsonResponse
 import json
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    print(request.GET)
-    body = request.body
+    model_data = Product.objects.all().order_by('?').first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['headers']=dict(request.headers)
-    data['content_type']=request.content_type
-    data['query_params']=dict(request.GET)
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    else:
+        data['message'] = 'No products found'
     return JsonResponse(data)
