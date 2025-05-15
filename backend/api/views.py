7f14a2de56eeb2d819820ab3c38ef869f0e0341e
Ishan Singh <isingh@indxx.com>
2025-05-15 11:40:20 +0530

    Django Model Instance to Dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,13 @@
 from django.http import JsonResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data)
     else:
         data['message'] = 'No products found'
     return JsonResponse(data)
