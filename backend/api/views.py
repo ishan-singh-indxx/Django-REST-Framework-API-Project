4d9966068287464d968c35f4edc2f8f6a6fc0273
Ishan Singh <isingh@indxx.com>
2025-05-15 11:06:49 +0530

    Echoing GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello World, this is your Django API Response!"})
+    print(request.GET)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers']=dict(request.headers)
+    data['content_type']=request.content_type
+    data['query_params']=dict(request.GET)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,8 @@
 import requests
 
 # endpoint = 'https://httpbin.org/status/200'
-endpoint ='http://127.0.0.1:8000/api'
+endpoint ='http://127.0.0.1:8000/api/'
 
-get_resonse = requests.get(endpoint)
+get_resonse = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello there"})
+print(get_resonse.status_code) 
 print(get_resonse.json()) 
