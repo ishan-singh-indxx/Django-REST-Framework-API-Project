ded4d61d5dcb93f5e86594a12abc0c44dcec1498
Ishan Singh <isingh@indxx.com>
2025-05-15 13:03:17 +0530

    DRF Model Serializers

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,4 +5,4 @@ endpoint ='http://127.0.0.1:8000/api/'
 
 get_resonse = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello there"})
 print(get_resonse.status_code) 
-print(get_resonse.json()) 
+print(get_resonse.text) 
